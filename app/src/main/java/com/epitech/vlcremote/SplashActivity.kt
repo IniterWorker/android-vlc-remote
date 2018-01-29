fac4b4db13b8f31bb2361031baa58ec23302db7a
Walter Bonetti <walter.bonetti@epitech.eu>
2018-01-29 15:59:49 +0100

    Change Kotlin import and add retrofit, gson

---
+++ app/src/main/java/com/epitech/vlcremote/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.epitech.vlcremote
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by bonett_w on 1/29/18.
+ */
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // start the home activity
+
+        val intent = Intent(this, HomeActivity::class.java)
+
+        startActivity(intent)
+
+        finish()
+    }
+}
