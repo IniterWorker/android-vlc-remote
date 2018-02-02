a379fa63a0c296da19c5f33f20169205051d0ae2
IniterWorker <walter.bonetti@epitech.eu>
2018-02-02 08:12:48 +0100

    clean code player fragment

--- app/src/main/java/com/epitech/vlcremote/fragments/PlayerFragment.kt
+++ app/src/main/java/com/epitech/vlcremote/fragments/PlayerFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
 import com.epitech.vlcremote.R
+import com.epitech.vlcremote.services.VLCService
 import kotlinx.android.synthetic.main.fragment_player.view.*
 
 /**
@@ -16,8 +17,10 @@ import kotlinx.android.synthetic.main.fragment_player.view.*
 
 class PlayerFragment() : Fragment() {
 
+    private var service: VLCService? = null;
+
     companion object {
-        fun newInstance() : PlayerFragment {
+        fun newInstance(): PlayerFragment {
             return PlayerFragment()
         }
     }
@@ -27,23 +30,30 @@ class PlayerFragment() : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-       val view: LinearLayout = inflater!!.inflate(R.layout.fragment_player, container, false) as LinearLayout
+        val view: LinearLayout = inflater!!.inflate(R.layout.fragment_player, container, false) as LinearLayout
 
         // TODO: write all user interface
         with(view) {
-            remote_back.setOnClickListener {
-                Toast.makeText(context, "Remote Back", Toast.LENGTH_SHORT).show()
-            }
-
-            remote_start.setOnClickListener {
-                Toast.makeText(context, "Remote Play/Pause", Toast.LENGTH_SHORT).show()
-            }
-
-            remote_next.setOnClickListener {
-                Toast.makeText(context, "Remote Next", Toast.LENGTH_SHORT).show()
-            }
+            remote_back.setOnClickListener { onClickBack() }
+            remote_start.setOnClickListener { onClickStart() }
+            remote_next.setOnClickListener { onClickNext() }
         }
 
         return view
+    }
+
+    private fun onClickBack() {
+        Toast.makeText(context, "Remote Back", Toast.LENGTH_SHORT).show()
+        // TODO : Service back
+    }
+
+    private fun onClickStart() {
+        Toast.makeText(context, "Remote Play/Pause", Toast.LENGTH_SHORT).show()
+        // TODO : Service toggle Start / Stop
+    }
+
+    private fun onClickNext() {
+        Toast.makeText(context, "Remote Next", Toast.LENGTH_SHORT).show()
+        // TODO : Service next
     }
 }
