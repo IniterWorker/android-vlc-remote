160e92155f7a9dbad5d55dafd1ee3cd33f36ebf8
IniterWorker <walter.bonetti@epitech.eu>
2018-02-15 07:43:56 +0100

    fix mistake stash apply

--- app/src/main/java/com/epitech/vlcremote/fragments/PlayerFragment.kt
+++ app/src/main/java/com/epitech/vlcremote/fragments/PlayerFragment.kt
@@ -159,16 +159,4 @@ class PlayerFragment() : TabFragment(), RemoteControllerView.OnRemoteControllerA
     override fun refresh() {
         Log.d(TAG, "start refresh...")
     }
-
-    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onStartTrackingTouch(p0: SeekBar?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onStopTrackingTouch(p0: SeekBar?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
