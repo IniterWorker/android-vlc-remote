5a04fbb570bd77bd4e3d1f4173f3014a82fe2441
IniterWorker <walter.bonetti@epitech.eu>
2018-02-15 07:45:09 +0100

    optimize import

--- app/src/main/java/com/epitech/vlcremote/adapters/PlaylistRecyclerViewAdapter.kt
+++ app/src/main/java/com/epitech/vlcremote/adapters/PlaylistRecyclerViewAdapter.kt
@@ -2,7 +2,6 @@ package com.epitech.vlcremote.adapters
 
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
